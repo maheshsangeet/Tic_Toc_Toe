663b1250ccd4b6663d83c34f5471bf09525f1b49
mahesh <maheshsangeet0@gmail.com>
2021-10-05 00:11:04 +0530

    player side completed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const boxes = document.querySelectorAll('.parent div');
 const boxesArray = [1,2,3,4,5,6,7,8,9];
 let playerMoves = '';
-let computerMoves = '';
+// let playerSelectedBox = '';
+
 
 
 // ******* player move ******* //
@@ -13,8 +14,15 @@ for (let i = 0; i < boxes.length; i++) {
         e.target.style.backgroundColor = 'red';
         boxesArray.splice(index,1);
         playerMoves += (e.target.dataset.name);
-        console.log(playerMoves)
+        console.log(playerMoves+'player moves')
 
+        if(playerMoves.length == 3){
+          playerMovesThree(e); 
+        }else if(playerMoves.length == 4) {
+          playerMovesFour(e);
+        }else if (playerMoves.length == 5){
+          console.log('draw');
+        }
 
         // computer move invoked
         setTimeout(()=> {
@@ -25,6 +33,8 @@ for (let i = 0; i < boxes.length; i++) {
 }
 
 // ******* computer move ******* //
+
+let computerMoves = '';
 function computerPlay() {
         
     const randomPick = Math.floor(Math.random()*10)%boxesArray.length;
@@ -33,9 +43,57 @@ function computerPlay() {
     boxes[(indexValue-1)].style.backgroundColor = 'green';
     boxesArray.splice((randomPick),1);
     computerMoves += indexValue;
-    console.log(computerMoves)
-
-        
+    console.log(computerMoves+'compter moves')      
 }
 
+
+let moves = ['123','456','789','147','258','369','159','357'];
+// let res = '';
+
+function playerMovesThree(e) {
+  
+    for(let i=0; i<moves.length; i++) {
+      console.log(moves.length-i)
+      let res = '';
+      for(let j=0; j<playerMoves.length; j++) {
+       console.log(j+'inner moves')
+
+        if(playerMoves.includes(moves[i][j])) {
+          res += moves[i][j];
+        } 
+      }
+      console.log(res+'rsuljbhmj')
+      if(res.length ==3){
+        console.log('winner');
+        break;
+      }
+      res = '';
+      
+    }
+
+  
+}
+function playerMovesFour(e) {
+
+    for(let i=0; i<moves.length; i++) {
+      console.log(moves.length-i)
+      let res = '';
+      for(let j=0; j<playerMoves.length; j++) {
+       console.log(j+'inner moves')
+
+        if(playerMoves.includes(moves[i][j])) {
+          res += moves[i][j];
+        } 
+      }
+      console.log(res+'rsuljbhmj')
+      if(res.length ==3){
+        console.log('winner');
+        break;
+      }
+      res = '';
+      
+    }
+
+  
+}
 
