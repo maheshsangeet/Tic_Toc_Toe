e3f171c1643d26789f121883ae6d94cd90746d32
mahesh <maheshsangeet0@gmail.com>
2021-10-05 13:48:16 +0530

    computer moves added

--- js/app.js
+++ js/app.js
@@ -1,99 +1,109 @@
 const boxes = document.querySelectorAll('.parent div');
 const boxesArray = [1,2,3,4,5,6,7,8,9];
-let playerMoves = '';
-// let playerSelectedBox = '';
+const moves = ['123','456','789','147','258','369','159','357'];
+let playerSelected = '';
 
 
+// ******* player moves ******* //
+function playerMoves () {
 
-// ******* player move ******* //
-for (let i = 0; i < boxes.length; i++) {
+  for (let i = 0; i < boxes.length; i++) {
     boxes[i].addEventListener('click',(e)=> {
 
-        const index = boxesArray.indexOf(parseInt(e.target.dataset.name));
+      const index = boxesArray.indexOf(parseInt(e.target.dataset.name));
 
-        e.target.style.backgroundColor = 'red';
-        boxesArray.splice(index,1);
-        playerMoves += (e.target.dataset.name);
-        console.log(playerMoves+'player moves')
+      e.target.style.backgroundColor = 'red';
+      boxesArray.splice(index,1);
+      playerSelected += (e.target.dataset.name);
+      // console.log(playerSelected+'player moves')
 
-        if(playerMoves.length == 3){
-          playerMovesThree(e); 
-        }else if(playerMoves.length == 4) {
-          playerMovesFour(e);
-        }else if (playerMoves.length == 5){
-          console.log('draw');
+
+      if(playerSelected.length == 3 || playerSelected.length == 4 ){
+        playerMovesCheck(e); 
+      }else if (playerSelected.length == 5){
+        console.log('draw');
+      }
+
+
+      // computer moves invoked
+      setTimeout(()=> {
+        computerMoves();
+
+        if(computerSelected.length == 3 || computerSelected.length == 4 ){
+          computerMovesCheck(e); 
         }
-
-        // computer move invoked
-        setTimeout(()=> {
-            computerPlay();
-        },1000)
-
+          
+      },1000)
+      
     })
+  }
 }
+playerMoves();
 
-// ******* computer move ******* //
 
-let computerMoves = '';
-function computerPlay() {
+// ******* computer moves ******* //
+
+let computerSelected = '';
+function computerMoves() {
         
     const randomPick = Math.floor(Math.random()*10)%boxesArray.length;
     const indexValue = boxesArray[randomPick];
 
     boxes[(indexValue-1)].style.backgroundColor = 'green';
     boxesArray.splice((randomPick),1);
-    computerMoves += indexValue;
-    console.log(computerMoves+'compter moves')      
+    computerSelected += indexValue;
+
+    // console.log(computerSelected+'computer moves')      
 }
 
 
-let moves = ['123','456','789','147','258','369','159','357'];
-// let res = '';
-
-function playerMovesThree(e) {
+// ******* checking box selected by player ****** //
+function playerMovesCheck(e) {
   
-    for(let i=0; i<moves.length; i++) {
-      console.log(moves.length-i)
-      let res = '';
-      for(let j=0; j<playerMoves.length; j++) {
-       console.log(j+'inner moves')
+  for(let i=0; i<moves.length; i++) {
+    // console.log(moves.length-i)
 
-        if(playerMoves.includes(moves[i][j])) {
-          res += moves[i][j];
-        } 
-      }
-      console.log(res+'rsuljbhmj')
-      if(res.length ==3){
-        console.log('winner');
-        break;
-      }
-      res = '';
-      
+    let check = '';
+    for(let j=0; j<playerSelected.length; j++) {
+    //  console.log(j+'inner moves')
+      if(playerSelected.includes(moves[i][j])) {
+        check += moves[i][j];
+      } 
     }
 
-  
+    // console.log(check+'player  ')
+    if(check.length ==3){
+      console.log('winner');
+    }else if(check.length ==4) {
+      console.log('winner');
+    }
+
+  }
+
 }
-function playerMovesFour(e) {
 
-    for(let i=0; i<moves.length; i++) {
-      console.log(moves.length-i)
-      let res = '';
-      for(let j=0; j<playerMoves.length; j++) {
-       console.log(j+'inner moves')
+// ******* checking box selected by computer ****** //
 
-        if(playerMoves.includes(moves[i][j])) {
-          res += moves[i][j];
-        } 
-      }
-      console.log(res+'rsuljbhmj')
-      if(res.length ==3){
-        console.log('winner');
-        break;
-      }
-      res = '';
-      
+function computerMovesCheck(e) {
+  
+  for(let i=0; i<moves.length; i++) {
+    // console.log(moves.length-i)
+
+    let check = '';
+    for(let j=0; j<computerSelected.length; j++) {
+    //  console.log(j+'computer inner moves')
+      if(computerSelected.includes(moves[i][j])) {
+        check += moves[i][j];
+      } 
     }
 
-  
+    // console.log(check+'computer')
+    if(check.length ==3){
+      console.log('winner computer');
+    }else if(check.length ==4) {
+      console.log('winner computer');
+    }
+  }
+
 }
 
